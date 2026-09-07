5b3a80902e73e1c29715b642af8cb93513663aea
jeff <jeff.kwak@gmail.com>
2026-09-07 18:25:06 -0400

    placeholders removed

--- js/site-footer.js
+++ js/site-footer.js
@@ -4,7 +4,7 @@ customElements.define('site-footer', class extends HTMLElement {
       <span>&copy; 2026 Long Ago Studios</span>
       <span class="socials">
         <a href="https://www.youtube.com/@LongAgoStudios">YouTube</a>
-        <a href="#">Bluesky</a>
+        <a href="https://bsky.app/profile/jeff-kwak.bsky.social">Bluesky</a>
         <!-- <a href="#">Discord</a> -->
       </span>
     `;
